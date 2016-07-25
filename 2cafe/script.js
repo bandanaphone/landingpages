c3f6dd426e97acceb14dabe9d9b39d2d3a616f5b
Moya J <mjohnsen@gmx.com>
2016-07-24 22:11:59 -0500

    Added Cafe

---
+++ 2cafe/script.js
@@ -0,0 +1,10 @@
+document.getElementById('menu_sel').addEventListener('click', function(){
+document.getElementById('pointer').style.marginLeft = '18%';
+document.getElementById('scroller').style.marginLeft = '-100%';	
+});
+
+document.getElementById('wel_sel').addEventListener('click', function(){
+document.getElementById('pointer').style.marginLeft = '10px';
+document.getElementById('scroller').style.marginLeft = '0px';	
+});
+
